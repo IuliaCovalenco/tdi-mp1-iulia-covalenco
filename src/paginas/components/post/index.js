2bbafee1fcdfa359514d0ec4801fe3cd603c2cdc
IuliaCovalenco <61017754+IuliaCovalenco@users.noreply.github.com>
2020-11-11 22:31:11 +0000

    git add

--- src/paginas/components/post/index.js
+++ src/paginas/components/post/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import Card from "react-bootstrap/esm/Card";
-import Col from "react-bootstrap/esm/Col";
 import '../../../App.css';
 import Button from 'react-bootstrap/Button';
-import { CardDeck } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getPosts } from '../../../store/posts//actions/index.js';
+import CardDeck from 'react-bootstrap/esm/CardDeck'
+import Col from 'react-bootstrap/esm/Col'
 
 
+export class Cards extends Component {
 
-
-
-export default ({ name, layout, cmc, gameFormat, artist, id }) => (
-  <Col lg={4} className="mt-5"> 
+  render() {
+    const { id, name, layout, cmc, artist, gameFormat } = this.props;
+   
+    return (
+      <Col lg={4} className="mt-5"> 
   <Card className="card h-100" style={{width: '23rem', color: 'black'}}>
   <CardDeck>
       <Card.Body className="card h-100" style={{width: '23rem'}}>
@@ -27,4 +31,16 @@ export default ({ name, layout, cmc, gameFormat, artist, id }) => (
       </Button></Link>
   </Card>
 </Col>
-)
+    );
+  }
+}
+
+
+const mapStateToProps = state => ({
+  card: state.cards.card
+});
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(Cards);
